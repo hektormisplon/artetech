58720ee06a3679a64534c03f8e1f6d9bf6c715cd
h <hektor.misplon@gmail.com>
2019-12-27 23:17:10 +0100

    Add JWT authentication

--- site/config/environments/development.php
+++ site/config/environments/development.php
@@ -11,6 +11,10 @@ Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 
+// JWT config
+Config::define('JWT_AUTH_SECRET_KEY', 'Invalid JWT configuration');
+Config::define('JWT_AUTH_CORS_ENABLE', true);
+
 ini_set('display_errors', '1');
 
 // Enable plugin and theme updates and installation from the admin
